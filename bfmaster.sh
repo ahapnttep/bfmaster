6158036cd027c492082d064aef787384f6ec37df
Ana Ritter <anaclara.ritter@pm.me>
2025-03-17 16:42:46 -0300

    Create bfmaster.sh
    sh file containing the main code

---
+++ bfmaster.sh
@@ -0,0 +1,327 @@
+### Aleister,
+### Espero que isso te force a reinstalar teu Arch ('I use Arch BTW')
+### Essa atrocidade foi codada visando a uma de nossas piadas internas
+### Para todos os efeitos, com ou sem ela, é sempre bom te relembrar
+### Sobre o quanto eu te odeio u_u
+### E feliz aniversário <3 (21/02/2025 aqui)
+
+### O segredo é:
+# ++++++++++[>+>+++>+++++++>++++++++++<<<<-]>>>-----.<++.>>-.++++++++++++.+++..-------------.+++++++++++++++.-------------------.<<.>>+++.+.--.++++++++++++.-----------.+++++.---.+++.------.--.++.--.++++++++++++++.<<.>>-----------.+.++++++++++++++.+.-------------------.<<.>>++++++++++++.--------.+++++++++.+++++.------------------.++++++.--.++++++++.<<.>>----.++++.+++.----.---.------.--.<<.>>+++++++++++++.+.<<.>>+++.-------------.--.++++++++++++.-.------.---.--.++++++.++++.--------.+++++++++.++++++.-----.<<.>>------.+++++.---------.++++++++++++.++++.------------.+++++++++++++.-------.------------.++++++++++++.<<++++++++++++.------------.>>+.-.+++.<<.>>--.---------------.+++++++++++++++++.++.---------------.<<.>>-.+++++++++++.<<.>>-----------.+.++++++++++++++.+.-----------.+++++.-------------.+++++++++++++++++++.-------------------.+++++++++++++++++.---------.++++++.<<++++++++++++.------------.>>-----------.---.<<.>>+++++++++++++++.++.---------.++++.------------.+++++++++++++++++++++++++.-----------------.--------.<<.>>.+++++.-.+++++++++++++++.-----------.+++++++++++++.---------------------.<<.>>+++.+.<<.>.>+++++++++.-------------.<<.>>++++.++++++++.<<.>>+++++.-------------.+++++++.-----------.++.--.++++++++++++++.<<.>>--------------.<<.>.>+++++++++++.-------.++++.++++++++++.+.---------------.+++++++++++++.<<++++++++++++++.--------------.>.>--------------.+.++++++++.------------.++++++++.++++++++++.<<++++++++++++.------------.>>--------------.+++++++++++++++++.-----------------.+++++++++.++++++.+.--------------------.++++++++.++++++++++.<<.>>----------------.++++++++++++.-.++++++.---------------.++++++++++++++.+.-------------------.++.++++++++++++.----------.++++++++++++++.<<.>>-------------.+++++++++++++++.-.+.---.-----------------.++++++++++++++++++.<<.>>---------------.+.+++++++++++++++++.-----------------.+++++++++++++.-----------------.++++++++++++++.<<.>>++++.--------------.+++++++++++++.<<.>>--.++.-------------.--.++.-.+++++.-----.---.++++++++++++++++++.<<.>>---------------.---.<<.>>++++++++.+++++.++++++.---------------.++.+++++++++++.-----------------.+++++++++++.<<.>>---------.++++++++++++.-.+++++.++.---------.++++++++.-------------------.<<.>>++++.<<.>>----.++.++.++++.+++++++++++.-------------------.++.--.++++++++++++++.<<.>>-----------.+++++++++++.++++.<<.>+++++++++++++++++++.>--------------.+++++++++++++.-----.++.++++.<<.>>--------------.++++++++++++++.+.<+++++++++++++.+.+++.+++++++.-------.--.++++++.-----.>-----.++++.<<++++++++++++++.
+
+### MEU MOMENTO DE BRILHAR
+### Faz download/copia e cola isso num arquivo .sh || <umnomemuitofodaquetuescolheuadedo>.sh
+### chmod +x <umnomemuitofodaquetuescolheuadedo>.sh
+### ./<umnomemuitofodaquetuescolheuadedo>.sh
+### Seja feliz
+
+##############################################
+#                                            #
+##                                          ##
+###                                        ###
+#### THE MOST PICA SETUP YOU'VE EVER SEEN ####
+###                                        ###
+##                                          ##
+#                                            #
+##############################################
+
+# !/bin/bash
+
+INSTALL_DIR="$HOME/s2"
+SCRIPT_NAME="brainfuckmaster.sh"
+MESSAGE_BF_NAME="secret.txt"
+SCRIPT_PATH="$INSTALL_DIR/$SCRIPT_NAME"
+MESSAGE_BF_PATH="$INSTALL_DIR/$MESSAGE_BF_NAME"
+ALIAS_NAME="bfmaster"
+
+MESSAGE_BF="++++++++++[>+>+++>+++++++>++++++++++<<<<-]>>>-----.<++.>>-.++++++++++++.+++..-------------.+++++++++++++++.-------------------.<<.>>+++.+.--.++++++++++++.-----------.+++++.---.+++.------.--.++.--.++++++++++++++.<<.>>-----------.+.++++++++++++++.+.-------------------.<<.>>++++++++++++.--------.+++++++++.+++++.------------------.++++++.--.++++++++.<<.>>----.++++.+++.----.---.------.--.<<.>>+++++++++++++.+.<<.>>+++.-------------.--.++++++++++++.-.------.---.--.++++++.++++.--------.+++++++++.++++++.-----.<<.>>------.+++++.---------.++++++++++++.++++.------------.+++++++++++++.-------.------------.++++++++++++.<<++++++++++++.------------.>>+.-.+++.<<.>>--.---------------.+++++++++++++++++.++.---------------.<<.>>-.+++++++++++.<<.>>-----------.+.++++++++++++++.+.-----------.+++++.-------------.+++++++++++++++++++.-------------------.+++++++++++++++++.---------.++++++.<<++++++++++++.------------.>>-----------.---.<<.>>+++++++++++++++.++.---------.++++.------------.+++++++++++++++++++++++++.-----------------.--------.<<.>>.+++++.-.+++++++++++++++.-----------.+++++++++++++.---------------------.<<.>>+++.+.<<.>.>+++++++++.-------------.<<.>>++++.++++++++.<<.>>+++++.-------------.+++++++.-----------.++.--.++++++++++++++.<<.>>--------------.<<.>.>+++++++++++.-------.++++.++++++++++.+.---------------.+++++++++++++.<<++++++++++++++.--------------.>.>--------------.+.++++++++.------------.++++++++.++++++++++.<<++++++++++++.------------.>>--------------.+++++++++++++++++.-----------------.+++++++++.++++++.+.--------------------.++++++++.++++++++++.<<.>>----------------.++++++++++++.-.++++++.---------------.++++++++++++++.+.-------------------.++.++++++++++++.----------.++++++++++++++.<<.>>-------------.+++++++++++++++.-.+.---.-----------------.++++++++++++++++++.<<.>>---------------.+.+++++++++++++++++.-----------------.+++++++++++++.-----------------.++++++++++++++.<<.>>++++.--------------.+++++++++++++.<<.>>--.++.-------------.--.++.-.+++++.-----.---.++++++++++++++++++.<<.>>---------------.---.<<.>>++++++++.+++++.++++++.---------------.++.+++++++++++.-----------------.+++++++++++.<<.>>---------.++++++++++++.-.+++++.++.---------.++++++++.-------------------.<<.>>++++.<<.>>----.++.++.++++.+++++++++++.-------------------.++.--.++++++++++++++.<<.>>-----------.+++++++++++.++++.<<.>+++++++++++++++++++.>--------------.+++++++++++++.-----.++.++++.<<.>>--------------.++++++++++++++.+.<+++++++++++++.+.+++.+++++++.-------.--.++++++.-----.>-----.++++.<<++++++++++++++."
+
+# Stores the script and message in your heart
+mkdir -p "$INSTALL_DIR"
+if [ ! -f "$SCRIPT_PATH" ]; then
+    cp "$0" "$SCRIPT_PATH"
+    chmod +x "$SCRIPT_PATH"
+fi
+
+# Save the Brainfuck message
+echo "$MESSAGE_BF" > "$MESSAGE_BF_PATH"
+chmod 644 "$MESSAGE_BF_PATH"
+
+# Shell config file
+BASH_RC="$HOME/.bashrc"
+ZSH_RC="$HOME/.zshrc"
+
+# Blames you if I wasn't smart enough to detect your sh
+check_shell_warning() {
+    if [[ "$SHELL" != *"bash"* && "$SHELL" != *"zsh"* ]]; then
+        echo -e "\n\e[3m*Your shell ($SHELL) is not Bash or Zsh. Add the following alias manually to your shell's config file:*\e[0m"
+        echo "alias $ALIAS_NAME=\"$SCRIPT_PATH\""
+    fi
+}
+
+# Checks if the file exists and grants permissions to write on it
+touch "$BASH_RC" "$ZSH_RC"
+chmod 644 "$BASH_RC" "$ZSH_RC"
+
+# I won't be a double trouble
+add_alias() {
+    local file="$1"
+    if ! grep -q "alias $ALIAS_NAME=" "$file"; then
+        echo -e "\n# Brainfuck Master alias" >> "$file"
+        echo "alias $ALIAS_NAME=\"$SCRIPT_PATH\"" >> "$file"
+    else
+        echo "Alias '$ALIAS_NAME' already exists in $file EOF. No need to write anything"
+    fi
+}
+
+# Adds the most lovable alias ever to the sh config file
+add_alias "$BASH_RC"
+add_alias "$ZSH_RC"
+
+############################################
+#                                          #
+##                                        ##
+###                                      ###
+#### BEHOLD THE BRAINFUCK MASTER ITSELF ####
+###                                      ###
+##                                        ##
+#                                          #
+############################################
+
+# Replaces accented characters with unnaccented equivalents
+normalize_input() {
+    echo "$1" | sed -e 's/[áàãâ]/a/g' -e 's/[éèê]/e/g' -e 's/[íï]/i/g' \
+                   -e 's/[óòõô]/o/g' -e 's/[úùü]/u/g' -e 's/[ç]/c/g' \
+                   -e 's/[ÁÀÃÂ]/A/g' -e 's/[ÉÈÊ]/E/g' -e 's/[ÍÏ]/I/g' \
+                   -e 's/[ÓÒÕÔ]/O/g' -e 's/[ÚÙÜ]/U/g' -e 's/[Ç]/C/g'
+}
+
+# Displays the title of the selected option
+show_title() {
+    local title="$1"
+    local color="$2"
+    echo -e "\e[1;7m $title \e[0m"
+    echo ""
+}
+
+# Function to prompt for (kisses) the right key to return to the main menu
+return_to_menu() {
+    echo ""
+    echo -ne "Me dá um beijinho pra eu te levar de volta pro menu foda... "
+    while true; do
+        echo -ne "\e[1;35m:*\e[0m "
+        read -r choice
+        if [[ "$choice" == "*:" ]]; then
+            clear
+            break
+        else
+            echo -e "\e[3mNão foi isso que eu pedi, dá meu beijo direito\e[0m \e[1;35m:@\e[0m"
+        fi
+    done
+}
+
+# Function to display the Terms message, makes sense only if ma luv properly use the interpreter
+terms() {
+echo -e "\e[3mA exibição dos Termos estará disponível após a execução da opção 1, 'Interpreter', e posterior inserção do 'segredo' disponível no início do arquivo, este sendo uma chave única de autenticação para a devida exibição do conteúdo.\e[0m"
+}
+
+
+# Brainfuck interpreter
+interpret() {
+    code="$1"
+
+    if [[ "$code" =~ [a-zA-Z0-9] ]]; then # If the code contains non-Brainfuck characters, handle the error
+        echo -e "\n\e[3mEu sei que isso não é Brainfuck... e exijo brainfucks.\e[0m\n"
+        echo -e "\e[1;33ma.\e[0m Tentar novamente sem falhar miseravelmente em me trollar"
+        echo -e "\e[1;33mb.\e[0m Revisitar o menu"
+
+        while true; do # Prompt for a valid choice
+            echo -ne "\e[1;32m> \e[0m"
+            read -r choice
+            choice_lower=$(echo "$choice" | tr '[:upper:]' '[:lower:]')
+            case "$choice_lower" in
+                a)
+                    echo "Sou tua especialista em bf (b̶o̶y̶f̶r̶i̶e̶n̶d̶ brainfuck), portanto use-me como tradutora oficial. "
+                    echo ""
+                    echo -ne "\e[1;32m> \e[0m"
+                    read -r bf_code
+                    interpret "$bf_code"
+                    return
+                    ;;
+                b)
+                    return
+                    ;;
+                *)
+                    echo -e "\n\e[3mIsso não é nem a nem b. Faz direito.\e[0m"
+                    ;;
+            esac
+        done
+    fi
+
+    memory=()
+    for ((i = 0; i < 30000; i++)); do memory[i]=0; done
+    ptr=0
+    output=""
+
+    i=0
+    while ((i < ${#code})); do
+        case "${code:i:1}" in
+            '>')
+                ((ptr++))
+                ;;
+            '<')
+                ((ptr--))
+                ;;
+            '+')
+                ((memory[ptr]++))
+                ;;
+            '-')
+                ((memory[ptr]--))
+                ;;
+            '.')
+                output+=$(printf "\\$(printf "%03o" ${memory[ptr]} )")
+                ;;
+            '[')
+                if ((memory[ptr] == 0)); then
+                    depth=1
+                    while ((depth > 0)); do
+                        ((i++))
+                        case "${code:i:1}" in
+                            '[') ((depth++)) ;;
+                            ']') ((depth--)) ;;
+                        esac
+                    done
+                fi
+                ;;
+            ']')
+                if ((memory[ptr] != 0)); then
+                    depth=1
+                    while ((depth > 0)); do
+                        ((i--))
+                        case "${code:i:1}" in
+                            '[') ((depth--)) ;;
+                            ']') ((depth++)) ;;
+                        esac
+                    done
+                fi
+                ;;
+        esac
+        ((i++))
+    done
+    echo ""
+    echo -e "\e[1;7mBRAINFUCK OUTPUT\e[0m"
+    echo -e "\n\e[1;1m\\e[0;3m$output\e[0;1m\\e[0m"
+}
+
+# Brainfuck encoder method
+encode_brainfuck() {
+    local input="$1"
+    local prev=0
+    local bf_code=""
+
+    for (( i=0; i<${#input}; i++ )); do
+        char="${input:$i:1}"
+        ascii_val=$(printf "%d" "'$char")
+        diff=$((ascii_val - prev))
+
+        if [[ $diff -gt 0 ]]; then
+            for (( j=0; j<$diff; j++ )); do bf_code+="+"; done
+        elif [[ $diff -lt 0 ]]; then
+            for (( j=0; j<$(( -diff )); j++ )); do bf_code+="-"; done
+        fi
+
+        bf_code+="."
+        prev=$ascii_val
+    done
+
+    echo "$bf_code"
+}
+
+# Run the encoder if it's option was selected
+run_encoder() {
+    echo "Digita ae o que tu quer traduzir pra depois me mandar pra eu decodificar, ou então pra tu testar se meu código funfa direito"
+    read -p $'\e[1;32m> \e[0m' user_input
+    if [[ "$user_input" =~ [áàãâéèêíïóòõôúùüçÁÀÃÂÉÈÊÍÏÓÒÕÔÚÙÜÇ] ]]; then
+echo -e "∘∘∘ \e[3mAkshually eu sabia que tu ia tentar burlar isso aqui. Boa sorte tentando forçar o uso de caracteres especiais. \e[1;35m8^B\e[0m \e[0m\n"
+    fi
+    normalized_input=$(normalize_input "$user_input")
+    brainfuck_code=$(encode_brainfuck "$normalized_input")
+    echo ""
+    echo -e "\e[1;7mBRAINFUCK ENCODED OUTPUT\e[0m"
+    echo "$brainfuck_code"
+    return_to_menu
+}
+
+# Main menu
+menu() {
+    while true; do
+        clear
+        echo -e "\e[1;7m♥ ♥ ♥ MENU MUITO DO FODA ♥ ♥ ♥\e[0m"
+        echo "1. Interpreter"
+        echo "2. Encoder"
+        echo "3. Terms"
+        echo -e "4. \e[1;31m<3\e[0m"
+        echo "5. Bye"
+        echo -e "\e[1mEscolha com sabedoria...\e[0m"
+        read -p $'\e[1;32m> \e[0m' option
+        echo ""
+
+        case $option in
+            1)
+                show_title "INTERPRETER" ""
+                echo "Sou tua especialista em bf (b̶o̶y̶f̶r̶i̶e̶n̶d̶ brainfuck), portanto use-me como tradutora oficial. "
+                echo ""
+                read -p $'\e[1;32m> \e[0m' bf_code
+                interpret "$bf_code"
+                return_to_menu
+                ;;
+            2)
+                show_title "ENCODER" ""
+                run_encoder
+                ;;
+            3)
+                show_title "TERMS" ""
+                terms
+                return_to_menu
+                ;;
+            4)
+                show_title "<3" "31" # Coz it's the colour of love
+                echo -e "\e[3mAna falou que você é 01 idiota.\e[0m \e[1;35m:p\e[0m"
+                return_to_menu
+                ;;
+            5)
+                show_title "BYE" "" # Qt goodbye animation
+                for i in {1..1}; do
+                    echo -ne "\e[1;35m\r('_^)ノ\e[0m"
+                    sleep 0.4
+                    echo -ne "\e[1;35m\r\r('_^)ゝ\e[0m"
+                    sleep 0.4
+                    echo -ne "\e[1;35m\r\r('_^)ノ\e[0m"
+                    sleep 0.4
+                done
+                echo ""
+                echo ""
+
+                # Se o alias foi adicionado corretamente, exibe a mensagem normal
+                if grep -q "alias $ALIAS_NAME=" "$BASH_RC" || grep -q "alias $ALIAS_NAME=" "$ZSH_RC"; then
+                    echo -e "\e[3mQuando me quiser de novo, só dizer 'bfmaster'. =)\e[0m"
+                else
+                    check_shell_warning # Chama a função se o alias não estiver no shell
+                fi
+
+                exec "$SHELL"
+                exit 0
+                ;;
+            *)
+                echo -e "\e[3mEu te dei um menu prontinho. Custa digitar alguma das opções dispostas?\e[0m" # Tome bronca
+                return_to_menu
+                ;;
+        esac
+    done
+}
+
+menu
+
+# Fico feliz que tenha ficado curioso o suficiente para ver o código até o final.
+# Caso não tivesse o feito, estragaria a surpresa.
+# https://www.youtube.com/watch?v=OJMylnasS48
+# <3
